cdfd4f2bc29aa1c601154101daf42f25607faca3
kwucslkm <kwucsani@naver.com>
2025-08-03 22:59:16 +0900

    이미지 크기 줄이기

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default async function RootLayout({ children }) {
   const res = await fetch('http://localhost:9999/topics');
-  const topics = res.json();
+  const topics = await res.json();
   return (
     <html>
       <body>
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <>
       <h2>Welcome</h2>
-        Hello, WEB!
-        <img src="/hello.png" alt ="hello"></img>
+        Hello, WEB!<br></br><br></br>
+        <img src="/hello.png" alt ="hello"  style={{ width: '100px' }}></img>
     </>
   );
 }
