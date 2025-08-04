66dbb33db2019db6d7239f305c3b4713f655078f
kwucslkm <kwucsani@naver.com>
2025-08-04 23:32:12 +0900

    새 글 생성 2025-0804

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,10 +1,13 @@
 // src/app/read/[id]/page.js
 
-export default async function Read({ params }) {
+export default async function Read(props) {
+  console.log(props.params.id);
+  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await res.json();
   return (
     <>
-      <h2>Read</h2>
-      parameters : {params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
